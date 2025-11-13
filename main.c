8b5bb105518e191b25d567211c9ee5c159ac8611
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-11-13 13:58:29 +0545

    init of windowMain

--- main.c
+++ main.c
@@ -5,6 +5,11 @@
 
 static void activate(GtkApplication *app,gpointer user_data) {
 
+    //Init of windowMain
+    GtkWidget *windowMain = gtk_application_window_new(app);
+    gtk_window_set_default_size(GTK_WINDOW(windowMain),600,600);
+    gtk_window_set_title(GTK_WINDOW(windowMain),"NotSoSpacy");
+
 }
 
 
