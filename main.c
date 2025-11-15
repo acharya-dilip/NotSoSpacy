8a12aa95133e6fabb89b2d836679158d3e285829
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-11-15 14:12:13 +0545

    removed the minimum size requirement for the windowMain

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ static void activate(GtkApplication *app,gpointer user_data) {
 
     //Init of windowMain
     GtkWidget *windowMain = gtk_application_window_new(app);
-    gtk_window_set_default_size(GTK_WINDOW(windowMain),300,300);
+    //gtk_window_set_default_size(GTK_WINDOW(windowMain),300,300);
     gtk_window_set_title(GTK_WINDOW(windowMain),"NotSoSpacy");
     gtk_window_present(GTK_WINDOW(windowMain));
 
