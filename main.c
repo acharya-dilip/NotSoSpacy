3469b463a09bacbc21df75e38085336e9b35a473
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-11-15 20:01:40 +0545

    locked the entryHour entry

--- main.c
+++ main.c
@@ -101,6 +101,7 @@ void screenAlarm() {
     //Init of entryhours
     GtkWidget *entryHour = gtk_entry_new();
     gtk_grid_attach(GTK_GRID(gridParent),entryHour,0,1,1,1);
+    gtk_editable_set_editable(GTK_EDITABLE(entryHour),FALSE);
     gtk_widget_add_css_class(entryHour,"entryHour");
     gtk_entry_set_alignment(GTK_ENTRY(entryHour),0.5);
     gtk_widget_set_hexpand(GTK_WIDGET(entryHour), FALSE);
