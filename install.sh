384ad56c0574b70f831e94f195459c6953ce6d66
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-11-17 02:51:27 +0545

    made some updates to the install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ sudo pacman -S cmake
 sudo pacman -S gtk
 sudo pacman -S gstreamer
 
+#Some BS
+echo "Yello You're prolly not gonna get hacked but let's find out!!"
+
 #Building the actual C Program or complin it ig
 cd build
 cmake ..
